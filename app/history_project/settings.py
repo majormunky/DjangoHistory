14ec726c532c89f193514f2ac798c593a1acb57e
Josh Bright <josh.bright@gmail.com>
2023-03-26 21:23:11 -0700

    Added core model, setup custom user model

--- app/history_project/settings.py
+++ app/history_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User Model
+AUTH_USER_MODEL = "core.CoreUser"
