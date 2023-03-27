15bfce2d6fef4f71378b1da4a3da14ed729ddd71
Josh Bright <josh.bright@gmail.com>
2023-03-26 21:27:16 -0700

    Added str method on the publication model

--- app/home/models.py
+++ app/home/models.py
@@ -5,3 +5,6 @@ class Publication(models.Model):
     name = models.CharField(max_length=512, unique=True)
     short_name = models.CharField(max_length=512, unique=True)
     code = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
