26424b5ab35c91e8fe0a960cd2b29d354cc499b3
Josh Bright <josh.bright@gmail.com>
2023-04-03 19:58:19 -0700

    Added Edition serializer

--- app/home/api/serializers.py
+++ app/home/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from home.models import Publication
+from home.models import Publication, Edition
 
 
 class PublicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publication
         fields = ["id", "name", "short_name", "code"]
+        read_only_fields = ["id"]
+
+
+class EditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Edition
+        fields = ["id", "publish_date", "page_count"]
         read_only_fields = ["id"]
