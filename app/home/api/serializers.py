002eb09a19a57be17d1f4d9e89df1dabbd78efca
Josh Bright <josh.bright@gmail.com>
2023-04-04 19:53:19 -0700

    Added page serializer

--- app/home/api/serializers.py
+++ app/home/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from home.models import Publication, Edition
+from home.models import Publication, Edition, Page
 
 
 class PublicationSerializer(serializers.ModelSerializer):
@@ -14,3 +14,9 @@ class EditionSerializer(serializers.ModelSerializer):
         model = Edition
         fields = ["id", "publish_date", "page_count"]
         read_only_fields = ["id"]
+
+
+class PageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Page
+        fields = ["id", "page_num", "url", "is_downloaded"]
