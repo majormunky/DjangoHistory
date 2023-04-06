e1466aa97866f382e1b9cfab6b21071d27e895d2
Josh Bright <josh.bright@gmail.com>
2023-04-05 20:26:14 -0700

    Added serializer for the core user model

---
+++ app/core/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from core.models import CoreUser
+
+
+class CoreUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CoreUser
+        fields = ["id", "first_name", "last_name"]
+        read_only_fields = ["id"]
